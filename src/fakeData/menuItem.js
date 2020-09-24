c44b3b3f4f8cb7b0d6e07499f61c0763e94cae26
rafsan <rafsanamin18@gmail.com>
2020-09-24 06:49:24 +0600

    fake data created

---
+++ src/fakeData/menuItem.js
@@ -0,0 +1,184 @@
+var menuItems = [
+  {
+    name: "Bagel and cream cheese",
+    price: 6.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/ZdKnBPX/breakfast4.png",
+    id: 201,
+    category: "breakfast",
+  },
+  {
+    name: "Breakfast sandwich",
+    price: 9.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/d5nkbK1/breakfast2.png",
+    id: 202,
+    category: "breakfast",
+  },
+  {
+    name: "Baked Chicken",
+    price: 10.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/5Wykjh8/breakfast3.png",
+    id: 203,
+    category: "breakfast",
+  },
+  {
+    name: "Eggs Benedict",
+    price: 8.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/BLp9b4c/breakfast1.png",
+    id: 204,
+    category: "breakfast",
+  },
+  {
+    name: "Toast Croissant Fried egg",
+    price: 19.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/92mwdf1/breakfast6.png",
+    id: 205,
+    category: "breakfast",
+  },
+  {
+    name: "Full Breakfast Fried Egg Toast ",
+    price: 3.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/bgByHQP/breakfast5.png",
+    id: 206,
+    category: "breakfast",
+  },
+  {
+    name: "Healthy Meal Plan",
+    price: 23.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/tMKhz2b/lunch6.png",
+    id: 207,
+    category: "lunch",
+  },
+  {
+    name: "Fried Chicken Bento",
+    price: 9.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/kJVb3qy/lunch5.png",
+    id: 208,
+    category: "lunch",
+  },
+  {
+    name: "Tarragon-Rubbed-Salmon",
+    price: 6.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/WPpcJ8c/lunch3.png",
+    id: 209,
+    category: "lunch",
+  },
+  {
+    name: "Indian Lunch",
+    price: 8.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/5nm6Dy4/lunch4.png",
+    id: 210,
+    category: "lunch",
+  },
+  {
+    name: "Beefsteak",
+    price: 15.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/QNG2k1p/lunch1.png",
+    id: 211,
+    category: "lunch",
+  },
+  {
+    name: "Honey-Soy-Glazed Salmon with Peppers",
+    price: 7.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/4ZgNVd3/lunch2.png",
+    id: 212,
+    category: "lunch",
+  },
+  {
+    name: "Salmon with Grapefruit and Lentil Salad",
+    price: 9.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/D9k4r1j/dinner6.png",
+    id: 213,
+    category: "dinner",
+  },
+  {
+    name: "Lemony Salmon Piccata",
+    price: 10.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/6JGcC8R/dinner2.png",
+    id: 214,
+    category: "dinner",
+  },
+  {
+    name: "Pork Tenderloin with Quinoa Pilaf",
+    price: 12.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/k2CQqs9/dinner5.png",
+    id: 215,
+    category: "dinner",
+  },
+  {
+    name: "French Fries with Cheese",
+    price: 8.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/N3GCcQh/dinner4.png",
+    id: 216,
+    category: "dinner",
+  },
+  {
+    name: "Garlic Butter Baked Salmon",
+    price: 6.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/2t8L5y8/dinner3.png",
+    id: 217,
+    category: "dinner",
+  },
+  {
+    name: "Baked Chicken",
+    price: 9.99,
+    info: "Lorem ipsum dolor sit amet.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur est enim asperiores officiis doloremque fuga error at quidem repellendus adipisci!",
+    img: "https://i.ibb.co/ZHNrKQS/dinner1.png",
+    id: 218,
+    category: "dinner",
+  },
+];
+
+export default menuItems;
